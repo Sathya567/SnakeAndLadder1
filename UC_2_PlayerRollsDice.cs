bedad37ab2208ff53170323cd17fcea6c37b1e95
BootCampBL <satyabhandari43@gmail.com>
2021-07-06 17:50:17 +0530

    [sathya] Add - player Rolls Dice

---
+++ UC_2_PlayerRollsDice.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeandladderStimulation
+{
+    class UC_2_PlayerRollsDice
+    {
+        public static void snake1()
+        {
+            int position = 0;
+            Console.WriteLine("Player is at position: " + position);
+            Random random = new Random();
+            int droll = 0;
+            while (true)
+            {
+                droll = random.Next(7);
+                if (droll != 0) break;
+
+            }
+            Console.WriteLine(droll);
+        }
+    }
+}
+
