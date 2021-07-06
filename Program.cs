8b310e050d03c73d4c06b0be8bdcd1b2c35d72c5
BootCampBL <satyabhandari43@gmail.com>
2021-07-06 18:57:18 +0530

    [sathya] Add - Playing snake and ladder with two players

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SnakeandladderStimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Welcome To Snake Ladder Game");
+            UC_1_PlayerStartingZero.snake();
+            UC_2_PlayerRollsDice.snake1();
+            UC_3_PlayerChecksForOption.snake2();
+            UC_4_PlayerWinningPosition.snake3();
+            UC_5_PlayerReachExactWinningPosition.snake4();
+            UC_6_NumberOfDicePlayed.snake5();
+            UC_7_PlayingWithTwoPlayers.snake6();
+        }
+        }
+    }
+
