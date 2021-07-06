e6b6e8cd3b5d7e452b0174cd938dfe3bfd19191f
BootCampBL <satyabhandari43@gmail.com>
2021-07-06 17:46:12 +0530

    [sathya] Add - Player starting position zero

---
+++ UC_1_PlayerStartingZero.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeandladderStimulation
+{
+    class UC_1_PlayerStartingZero
+    {
+            public static void snake()
+            {
+                int position = 0;
+                Console.WriteLine("Player is at position : " + position);
+            }
+        
+    }
+}
